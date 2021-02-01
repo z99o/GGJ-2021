0ffa63387b46bef574f472a77d3656b352f5638e
z99o <32403871+z99o@users.noreply.github.com>
2021-01-31 16:55:23 -0800

    Updated Game Master

--- Assets/Game_Master.cs
+++ Assets/Game_Master.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 //Manages all the overhead 
 public class Game_Master : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Character_Movement_3D controller;
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        controller = GameObject.Find("Player").GetComponent<Character_Movement_3D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(controller.m_health <= 0){
+            Game_Over();
+        }
+
+    }
+
+    void Game_Over(){
+        SceneManager.LoadScene("Main Menu");
     }
 }
