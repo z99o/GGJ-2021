9067b9b903926947f930623fe94325f408ab835c
Brendan Moore <moorbren@oregonstate.edu>
2021-01-31 20:21:19 -0800

    Added event system to main scene, fixing menus

--- Assets/Scenes/Main Menu/Scripts/MainMenu.cs
+++ Assets/Scenes/Main Menu/Scripts/MainMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame(){
         SceneManager.LoadSceneAsync(1);
     }
